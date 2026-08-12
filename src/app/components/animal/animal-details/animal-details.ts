a019af499d89cc37a210e80e8c602ff829a221d9
Lukestas <julianenriqueluquelopez@gmail.com>
2026-08-11 23:49:28 -0600

    feat: update app layout and implement animal details component

---
+++ src/app/components/animal/animal-details/animal-details.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animal-details',
+  imports: [],
+  templateUrl: './animal-details.html',
+})
+export class AnimalDetails {}
--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { Header } from '../header/header';
 import { AnimalList } from '../animal/animal-list/animal-list';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, AnimalList],
+  imports: [AnimalList],
   templateUrl: './home.html',
 })
 export default class Home {}
